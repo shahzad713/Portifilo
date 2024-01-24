1faee545b64dc7754ff4f1e07f9b72fc454542c6
Shahzad Munir <99446843+shahzad713@users.noreply.github.com>
2024-01-24 23:44:21 +0500

    Update ResumePage.js

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/shahzad.pdf";
+import pdf from "../../assets/Shahzad_Resume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
