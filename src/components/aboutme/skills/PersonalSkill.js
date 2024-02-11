d45517128426f930d480ee073a6f7c69f8887f23
shahzad713 <munirshahzad043@gmail.com>
2024-02-11 12:39:00 +0500

    update Technical Skills

--- src/components/aboutme/skills/PersonalSkill.js
+++ src/components/aboutme/skills/PersonalSkill.js
@@ -4,6 +4,14 @@ import { Container, Row, Col } from "react-bootstrap";
 import LightSpeed from "react-reveal/LightSpeed";
 
 export default function PersonalSkill() {
+  const skills = [
+    { name: "Communication Skills", color: "#fe3e57", value: 75 },
+    { name: "Problem Solving", color: "#ff8c2f", value: 80 },
+    { name: "Analytical Abilities", color: "#f1f965", value: 70 },
+    { name: "Organization", color: "#ff0173", value: 80 },
+    { name: "Creativity", color: "#39c4ff", value: 90 },
+  ];
+
   return (
     <div>
       <div className="mt-5 d-flex flex-row justify-content-center">
@@ -23,151 +31,41 @@ export default function PersonalSkill() {
       <div className="mt-4">
         <Container>
           <Row className="g-5">
-            <Col md={4}>
-              <h1
-                style={{ fontSize: "100px", color: "#fe3e57", fontWeight: 700 }}
-              >
-                75%
-              </h1>
-              <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
-                <div class="progress">
-                  <div
-                    role={"progressbar"}
-                    id={"com"}
-                    aria-valuenow={"75"}
-                    aria-valuemin={"0"}
-                    aria-valuemax={"100"}
-                    style={{ width: "75%" }}
-                  ></div>
-                </div>
-                <h2
+            {skills.map((skill, index) => (
+              <Col key={index} md={4}>
+                <h1
                   style={{
-                    color: "#fe3e57",
-                    fontSize: "25px",
-                    marginTop: "13px",
+                    fontSize: "100px",
+                    color: skill.color,
                     fontWeight: 700,
                   }}
                 >
-                  Communication Skills
-                </h2>
-              </div>
-            </Col>
-            <Col md={4}>
-              <h1
-                style={{ fontSize: "100px", color: "#ff8c2f", fontWeight: 700 }}
-              >
-                80%
-              </h1>
-              <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
-                <div class="progress">
-                  <div
-                    role={"progressbar"}
-                    id={"ps"}
-                    aria-valuenow={"81"}
-                    aria-valuemin={"0"}
-                    aria-valuemax={"100"}
-                    style={{ width: "81%" }}
-                  ></div>
+                  {skill.value}%
+                </h1>
+                <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
+                  <div className="progress">
+                    <div
+                      role="progressbar"
+                      id={`skill-${index}`}
+                      aria-valuenow={skill.value}
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: `${skill.value}%` }}
+                    ></div>
+                  </div>
+                  <h2
+                    style={{
+                      color: skill.color,
+                      fontSize: "25px",
+                      marginTop: "13px",
+                      fontWeight: 700,
+                    }}
+                  >
+                    {skill.name}
+                  </h2>
                 </div>
-                <h2
-                  style={{
-                    color: "#ff8c2f",
-                    fontSize: "25px",
-                    marginTop: "13px",
-                    fontWeight: 700,
-                  }}
-                >
-                  Problem Solving
-                </h2>
-              </div>
-            </Col>
-            <Col md={4}>
-              <h1
-                style={{ fontSize: "100px", color: "#f1f965", fontWeight: 700 }}
-              >
-                70%
-              </h1>
-              <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
-                <div class="progress">
-                  <div
-                    role={"progressbar"}
-                    id={"aa"}
-                    aria-valuenow={"77"}
-                    aria-valuemin={"0"}
-                    aria-valuemax={"100"}
-                    style={{ width: "77%" }}
-                  ></div>
-                </div>
-                <h2
-                  style={{
-                    color: "#f1f965",
-                    fontSize: "25px",
-                    marginTop: "13px",
-                    fontWeight: 700,
-                  }}
-                >
-                  Analytical Abilities
-                </h2>
-              </div>
-            </Col>
-            <Col md={4}>
-              <h1
-                style={{ fontSize: "100px", color: "#ff0173", fontWeight: 700 }}
-              >
-                80%
-              </h1>
-              <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
-                <div class="progress">
-                  <div
-                    role={"progressbar"}
-                    id={"org"}
-                    aria-valuenow={"87"}
-                    aria-valuemin={"0"}
-                    aria-valuemax={"100"}
-                    style={{ width: "87%" }}
-                  ></div>
-                </div>
-                <h2
-                  style={{
-                    color: "#ff0173",
-                    fontSize: "25px",
-                    marginTop: "13px",
-                    fontWeight: 700,
-                  }}
-                >
-                  Organization
-                </h2>
-              </div>
-            </Col>
-            <Col md={4}>
-              <h1
-                style={{ fontSize: "100px", color: "#39c4ff", fontWeight: 700 }}
-              >
-                90%
-              </h1>
-              <div style={{ paddingLeft: "50px", paddingRight: "50px" }}>
-                <div class="progress">
-                  <div
-                    role={"progressbar"}
-                    id={"cr"}
-                    aria-valuenow={"79"}
-                    aria-valuemin={"0"}
-                    aria-valuemax={"100"}
-                    style={{ width: "79%" }}
-                  ></div>
-                </div>
-                <h2
-                  style={{
-                    color: "#39c4ff",
-                    fontSize: "25px",
-                    marginTop: "13px",
-                    fontWeight: 700,
-                  }}
-                >
-                  Creativity
-                </h2>
-              </div>
-            </Col>
+              </Col>
+            ))}
           </Row>
         </Container>
       </div>
